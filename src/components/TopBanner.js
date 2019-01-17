144fe2ba947e74b3bf35454afc7412e87044be71
SarahL88 <sarahlei@bu.edu>
2019-01-17 15:42:17 -0500

    Added measurement questions, top banner, and custom buttons.

---
+++ src/components/TopBanner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+
+const topBanner = (props) => (
+  <View style={styles.topBanner}>
+    <Text style={styles.text}>
+      {props.content}
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  topBanner: {
+    justifyContent: 'center',
+    backgroundColor: '#00FF00',
+    height: 80
+  },
+  text: {
+    fontSize: 26,
+    alignSelf: "center"
+  }
+});
+
+export default topBanner;
