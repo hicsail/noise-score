2ab41688df7c9ca3bcfec29c812e90e6db455b4c
sidpremkumar <sid.premkumar@gmail.com>
2019-03-19 15:07:22 -0400

    heatmap work, starting to fix axios.post calls

--- android/app/src/main/java/com/noisescore/MainApplication.java
+++ android/app/src/main/java/com/noisescore/MainApplication.java
@@ -3,6 +3,7 @@ package com.noisescore;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.punarinta.RNSoundLevel.RNSoundLevel;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new MapsPackage(),
             new SvgPackage(),
             new RNSoundLevel(),
