1c773082af880fff99fb9f2e5f27f7d8e71e08ac
sidpremkumar <sid.premkumar@gmail.com>
2019-04-16 07:21:13 -0400

    starting to start dev on android

--- android/app/src/main/java/com/noisescore/MainApplication.java
+++ android/app/src/main/java/com/noisescore/MainApplication.java
@@ -13,7 +13,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.airbnb.android.react.maps.MapsPackage;
+//import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,7 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new SplashScreenReactPackage(),
-            new MapsPackage(),
             new MapsPackage(),
             new SvgPackage(),
             new RNSoundLevel(),
