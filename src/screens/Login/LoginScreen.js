479eb822d1175704d21270cab90663f40e3552e8
stathisKara <sdi1200057@di.uoa.gr>
2019-06-10 16:41:04 -0400

    Simple splaschreen implemented.

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -188,6 +188,7 @@ const styles = StyleSheet.create({
     },
     button: {
         marginTop: 20,
+        borderRadius: 30,
         // borderRadius:3,
         backgroundColor: '#323232'
     },
