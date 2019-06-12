89b3f96d85a57f9e02dc894984d9251bc47a470f
Stathis Karatsiolis <stathiskara@hotmail.com>
2019-06-12 11:54:22 -0400

    Completed reset password with input validation Transfered loging control to splashscreen. Flow is complete.
    Minor styling changes as well.

--- App.js
+++ App.js
@@ -6,10 +6,6 @@ import {
     createAppContainer,
     createSwitchNavigator
 } from 'react-navigation';
-
-import { Input, Text, Button } from 'react-native-elements';
-import { StyleSheet, View, ScrollView, AsyncStorage, Image, Alert } from 'react-native';
-// import { AsyncStorage } from "react-native";
 import MapScreen from './src/screens/Map/MapScreen';
 import MeasureScreen from './src/screens/Measure/MeasureScreen';
 import MeasureScreen1 from './src/screens/Measure/MeasureScreen1';
@@ -22,13 +18,18 @@ import LoginScreen from './src/screens/Login/LoginScreen';
 import SignUp from './src/screens/Login/SignUp';
 import SignUp2 from './src/screens/Login/SignUp2';
 import SignUp3 from './src/screens/Login/SignUp3';
-import resetPassword from './src/screens/Account/ResetPassword';
 import ForgotResetPassword from "./src/screens/Login/ForgotResetPassword";
-import axios from "axios";
+import ResetPassword from "./src/screens/Login/ResetPassword"
+import Splashscreen from "./src/screens/Login/Splashscreen";
 
 
 console.disableYellowBox = ["Unable to symbolicate"];
+const brightGreen = "#31BD4B";
 
+
+// --------------------- Stack Navigators ---------------------
+
+//Recordings stack navigator for its 4 screens
 const MeasureStack = createStackNavigator({
     Measure: MeasureScreen,
     Measure1: MeasureScreen1,
@@ -41,13 +42,13 @@ const AccountStack = createStackNavigator({
     Account1: AccountScreen,
     Account2: AccountPage,
     Account3: moreInfo,
-    // Account4: resetPassword,
 });
 
-const ForgotPassStack = createStackNavigator({
-    LoginScreen: LoginScreen,
-    ForgotPass: ForgotResetPassword
-});
+// const ForgotPassStack = createStackNavigator({
+//     Account: AccountStack,
+//     ForgotPass: ForgotResetPassword
+// });
+
 
 const MapStack = createStackNavigator({
     normalMap: {
@@ -56,14 +57,11 @@ const MapStack = createStackNavigator({
     }
 });
 
-const brightGreen = "#31BD4B";
-
 const home = createBottomTabNavigator(
     {
         Map: MapStack,
         Measure: MeasureStack,
         Account: AccountStack,
-        ForgotPass: ForgotPassStack
     },
     {
         defaultNavigationOptions: ({ navigation }) => ({
@@ -107,62 +105,57 @@ const home = createBottomTabNavigator(
     }
 );
 
-
 const SignUpStack = createStackNavigator({
     SignUp: SignUp,
     SignUp1: SignUp2,
     SignUp3: SignUp3,
 });
 
-export const login = createStackNavigator({
+const ForgotPassStack = createStackNavigator({
+    ForgotResetPassword: { screen: ForgotResetPassword },
+    ResetPassword: { screen: ResetPassword },
+});
+
+
+function getHeader(title = "", tintcolor = '#323232', headbgcolor = '#31BD4B') {
+    return {
+        title: title,
+        headerTintColor: tintcolor,
+        headerStyle: {
+            backgroundColor: headbgcolor
+        }
+    }
+}
+
+const login = createStackNavigator({
     SignIn: {
         screen: LoginScreen,
-        navigationOptions: {
-            header: null
-
-        }
+        navigationOptions: { header: null }
     },
     SignUp1: {
         screen: SignUp,
-        navigationOptions: {
-            title: "",
-            headerTintColor: '#323232',
-            headerStyle: {
-                backgroundColor: '#31BD4B'
-            },
-            // tintColor: '#cccc31'
-        }
+        navigationOptions: getHeader("Step 1/3"),
     },
     SignUp2: {
         screen: SignUp2,
-        navigationOptions: {
-            title: "",
-            headerTintColor: '#323232',
-            headerStyle: {
-                backgroundColor: '#31BD4B'
-            },
-        }
+        navigationOptions: getHeader("Step 2/3"),
+
     },
     SignUp3: {
         screen: SignUp3,
-        navigationOptions: {
-            title: "",
-            headerTintColor: '#323232',
-            headerStyle: {
-                backgroundColor: '#31BD4B'
-            },
-        }
+        navigationOptions: getHeader("Step 3/3"),
+
     },
     ForgotResetPassword: {
         screen: ForgotResetPassword,
-        navigationOptions: {
-            title: "Forgot Password",
-            headerTintColor: '#323232',
-            headerStyle: {
-                backgroundColor: '#31BD4B'
-            },
-        }
+        navigationOptions: getHeader("Forgot Password ?"),
+    },
+
+    ResetPassword: {
+        screen: ResetPassword,
+        navigationOptions: getHeader("Reset Password")
     }
+
 });
 
 
@@ -188,156 +181,20 @@ export const login = createStackNavigator({
 // };
 
 
-var signedIn = false;
-
-
-export const root = createSwitchNavigator({
-        SignedIn: {
-            screen: home
-        },
-        SignedOut: {
-            screen: login
-        }
+export const root = createStackNavigator({
+        SignedIn: { screen: home },
+        SignedOut: { screen: login }
     },
-    {
-        initialRouteName: signedIn ? "SignedIn" : "SignedOut"
-    }
 );
 
 
-const First = createAppContainer(root);
-
-// const home = createAppContainer(bottomNav);
-
-// class AuthLoading extends React.Component() {
-//     constructor(props){
-//         super(props);
-//         this.fetchStatus();
-//     }
-//
-//     fetchStatus = async () => {
-//         const loggedIn = await
-//     }
-// }
-
-
-export class Authentication extends React.Component {
-
-}
-
-export class SplashScreen extends React.Component {
-    performTimeConsumingTask = async () => {
-        return new Promise((resolve) =>
-            setTimeout(
-                () => {
-                    resolve('result')
-                },
-                2000
-            )
-        )
-    };
-
-    async AuthenticateUser() {
-        return await (
-            AsyncStorage.getItem("userData", null).then(async function (ret) {
-                let response = JSON.parse(ret);
-                console.log(ret);
-
-                if (ret) {
-                    if (response['authHeader'] != null) {
-                        // Verify user with sessions
-                        var authHeader = response['authHeader'];
-                        const header = {
-                            'Content-Type': 'application/json',
-                            'Authorization': authHeader
-                        };
-
-
-                        return axios.get('http://10.0.2.2:9000/api/sessions/my', { headers: header })
-                            .then(function () {
-                                return true;
-                            })
-                            .catch(function () {
-                                return false;
-                            })
-                    }
-
-                }
-                return false;
-            })
-
-        )
-    };
-
-    componentDidMount() {
-        // Preload data from an external API
-        // Preload data using AsyncStorage
-        // const data = await this.performTimeConsumingTask();
-
-        const data2 = this.AuthenticateUser();
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        console.log("data is " + data2);
-        if (data2) {
-            this.props.navigation.navigate('App');
-        }
-        else {
-            this.props.navigation.navigate("UserLogin")
-        }
-    }
-
-    render() {
-        return (
-            <View style={styles.viewStyles}>
-                <Image
-                    source={require('./assets/splash_logo.jpeg')}
-                    style={{
-                        flex: 1,
-                        alignSelf: 'stretch',
-                        width: undefined,
-                        height: undefined
-                    }}
-                />
-                <Image
-                    source={require('./assets/splash-image.jpeg')}
-                    style={{
-                        flex: 1,
-                        alignSelf: 'stretch',
-                        width: undefined,
-                        height: undefined
-                    }}
-                />
-            </View>
-        );
-    }
-}
-
-const styles = {
-    viewStyles: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        // backgroundColor: 'orange'
-    },
-    textStyles: {
-        color: 'white',
-        fontSize: 40,
-        fontWeight: 'bold'
-    }
-};
+// const First = createAppContainer(root);
 
 const Second = createSwitchNavigator({
-    Splash: SplashScreen,
-    App: First,
-    UserLogin: LoginScreen
+    Splash: Splashscreen,
+    App: home,
+    UserLogin: login
 });
-
-// export default createAppContainer(InitialNavigator);
-//
-// export default class App extends React.Component {
-//     render() {
-//         return <Second/>
-//     }
-// }
 
 
 export default createAppContainer(Second);
